71e9cea3657764872166a123268368d4347c6691
Anton <av.lushchay17@gmail.com>
2025-12-12 18:36:24 +0100

    fix: Update base path configuration to use dynamic repo name

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+const repoName = 'Christmas-Predictions';
+
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: '/Christmas-Predictions/',
+    base: process.env.NODE_ENV === 'production' ? `/${repoName}/` : '/',
     server: {
         port: 3000,
         host: true,
